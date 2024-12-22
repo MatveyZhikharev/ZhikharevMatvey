4adc418c79d6d7d2d347ea7f822b4f42f1291f97
Mattew-Kentor <matveyzhikharev@yandex.ru>
2024-12-22 17:36:59 +0300

    add new tests

---
+++ src/test/java/org/example/repository/CommentRepositoryImplTest.java
@@ -0,0 +1,112 @@
+package org.example.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.Application;
+import org.example.controller.ArticleController;
+import org.example.controller.ArticleFreemarkerController;
+import org.example.controller.CommentController;
+import org.example.service.ArticleService;
+import org.example.service.CommentService;
+import org.example.template.TemplateFactory;
+import org.flywaydb.core.Flyway;
+import org.jdbi.v3.core.Jdbi;
+import org.junit.jupiter.api.*;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import spark.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Testcontainers
+class CommentRepositoryImplTest {
+  @Container
+  public static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:13.5");
+  private static Jdbi jdbi;
+  private static Service service;
+
+  @BeforeAll
+  static void beforeAll() {
+    String postgresJdbcUrl = POSTGRES.getJdbcUrl();
+    Flyway flyway =
+        Flyway.configure()
+            .outOfOrder(true)
+            .locations("classpath:db/migrations")
+            .dataSource(postgresJdbcUrl, POSTGRES.getUsername(), POSTGRES.getPassword())
+            .load();
+    flyway.migrate();
+    jdbi = Jdbi.create(postgresJdbcUrl, POSTGRES.getUsername(), POSTGRES.getPassword());
+    service = Service.ignite();
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    jdbi.useTransaction(handle -> handle.createUpdate("DELETE FROM article").execute());
+    jdbi.useTransaction(handle -> handle.createUpdate("DELETE FROM comment").execute());
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    ArticleService articleService = new ArticleService(
+        new ArticleRepositoryImpl(jdbi)
+    );
+
+    CommentService commentService = new CommentService(new CommentRepositoryImpl(jdbi));
+
+    Application application = new Application(
+        List.of(
+            new ArticleController(service, articleService, objectMapper),
+            new CommentController(service, commentService, objectMapper),
+            new ArticleFreemarkerController(service, articleService, TemplateFactory.freeMarkerEngine())
+        )
+    );
+    application.start();
+    service.awaitInitialization();
+  }
+
+  @AfterEach
+  void afterEach() {
+    service.stop();
+    service.awaitStop();
+  }
+
+  @Test
+  void should400skippedArgument() throws IOException, InterruptedException {
+    HttpResponse<String> response = HttpClient.newHttpClient()
+        .send(
+            HttpRequest.newBuilder()
+                .POST(
+                    HttpRequest.BodyPublishers.ofString(
+                        """
+                            {}
+                            """
+                    )
+                )
+                .uri(URI.create("http://localhost:4567/api/comment"))
+                .build(),
+            HttpResponse.BodyHandlers.ofString(UTF_8)
+        );
+
+    assertEquals(400, response.statusCode());
+  }
+
+  @Test
+  void should404undefinedComment() throws IOException, InterruptedException {
+    HttpResponse<String> response = HttpClient.newHttpClient()
+        .send(
+            HttpRequest.newBuilder()
+                .DELETE()
+                .uri(URI.create("http://localhost:4567/api/comment/1"))
+                .build(),
+            HttpResponse.BodyHandlers.ofString(UTF_8)
+        );
+
+    assertEquals(404, response.statusCode());
+  }
+}
